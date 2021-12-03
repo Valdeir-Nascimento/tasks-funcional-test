2982d91ede13a18d6d35e6a40f671307a6931fe5
Valdeir Nascimento <valdeircn11@gmail.com>
2021-12-02 22:12:26 -0300

    Ajustes no selenium

--- src/test/java/br/ce/wcaquino/tasks/funcional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/funcional/TasksTest.java
@@ -17,7 +17,7 @@ public class TasksTest {
 //        WebDriver driver = new ChromeDriver();
         DesiredCapabilities cap = DesiredCapabilities.chrome();
         WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.106:4444/wd/hub"), cap);
-        driver.navigate().to("http://192.168.0.106:8001/tasks");
+        driver.navigate().to("http://192.168.0.106:9999/tasks");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return driver;
     }
